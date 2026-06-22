b31f159655076e5489255ea5939acc93fa33bd3f
qqqhester <romariobutakov22@gmail.com>
2026-06-22 23:08:34 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,19 @@
 from aiogram import Router
 from .profile import router as profile_router
 from .shop import router as shop_router
+from .market import router as market_router
+from .rivals import router as rivals_router
+from .admin import router as admin_router
+from .bonus import router as bonus_router
 
-# Создаем единый мастер-роутер для легкого импорта в корневом main.py
+# Единый мастер-роутер проекта
 router = Router()
-router.include_routers(profile_router, shop_router)
+
+router.include_routers(
+    admin_router,
+    profile_router,
+    shop_router,
+    market_router,
+    rivals_router,
+    bonus_router
+)
